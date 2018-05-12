71c5e4a943322dcfaf9bf8c2de67226f73f2dd6c
Tingan Ho <tingan87@gmail.com>
2018-05-12 16:56:48 +0200

    Adds diagnostics

---
+++ src/program/graphql/graphql_diagnostics.cpp
@@ -0,0 +1,8 @@
+// This code is auto generate. Don't edit it!
+#include "graphql_diagnostics.h"
+
+namespace flashpoint::program {
+
+    DiagnosticMessageTemplate D::Expected_0_but_got_1 = DiagnosticMessageTemplate { "Expected '{0}', but got '{1}'." };
+
+} // flashpoint::program
---
+++ src/program/graphql/graphql_diagnostics.h
@@ -0,0 +1,16 @@
+// This code is auto generate. Don't edit it!
+#ifndef DIAGNOSTICS_H
+#define DIAGNOSTICS_H
+
+#include <program/diagnostic.h>
+
+namespace flashpoint::program {
+
+class D {
+public:
+    static DiagnosticMessageTemplate Expected_0_but_got_1;
+};
+
+}
+
+#endif // DIAGNOSTICS_H
