d1ffdb4b66a6b125018b028b5e577ec6f77c3af5
Tingan Ho <tingan87@gmail.com>
2018-07-29 13:56:43 +0200

    chore(git): Fixes formatting of code

--- src/test/baseline_test_runner.h
+++ src/test/baseline_test_runner.h
@@ -13,88 +13,96 @@ using namespace boost::filesystem;
 using namespace flashpoint::lib;
 
 namespace flashpoint::test {
-    struct TestCase {
-        std::string name;
-        std::string aggregate_name;
-        std::string source;
-        std::string folder;
-        const path current_folder;
-        const path reference_folder;
-    };
 
-    struct RunOption {
-        std::experimental::optional<std::string> test;
-        std::experimental::optional<std::string> folder;
-    };
+struct TestCase {
+    std::string name;
+    std::string aggregate_name;
+    std::string source;
+    std::string folder;
+    const path current_folder;
+    const path reference_folder;
+};
 
-    class BaselineTestRunner {
-    public:
-        BaselineTestRunner();
+struct RunOption {
+    std::experimental::optional<std::string> test;
+    std::experimental::optional<std::string> folder;
+};
 
-        int
-        start_server();
+class BaselineTestRunner {
+public:
+    BaselineTestRunner();
 
-        void
-        define_http_tests(const RunOption &run_option);
+    int
+    start_server();
 
-        void
-        define_graphql_tests(const RunOption &run_option);
+    void
+    define_http_tests(const RunOption &run_option);
 
-        void
-        run(const RunOption &run_option);
+    void
+    define_graphql_tests(const RunOption &run_option);
 
-        void
-        accept_graphql_tests(const RunOption& run_option);
+    void
+    run(const RunOption &run_option);
 
-    private:
+    void
+    accept_graphql_tests(const RunOption& run_option);
 
-        std::size_t
-        current_line = 1;
+private:
 
-        int
-        child_pid = -1;
+    std::size_t
+    current_line = 1;
 
-        void
-        visit_tests_by_path(
-            const path& folder,
-            std::function<void(const TestCase& test_case)> callback,
-            bool delete_folder);
+    int
+    child_pid = -1;
 
-        void
-        visit_tests(
-            const path& test_folder,
-            std::vector<std::string>& tests,
-            std::function<void(const TestCase& test_case)> callback);
+    void
+    visit_tests_by_path(
+        const path& folder,
+        std::function<void(const TestCase& test_case)> callback,
+        bool delete_folder);
 
-        bool
-        assert_baselines(const char* current, const char* reference, std::stringstream& error_message);
+    void
+    visit_tests(
+        const path& test_folder,
+        std::vector<std::string>& tests,
+        std::function<void(const TestCase& test_case)> callback);
 
-        void
-        append_mutation_chunk(const std::string &message, bool is_insertion,
-                              TextWriter &tw);
+    bool
+    assert_baselines(
+        const char* current,
+        const char* reference,
+        std::stringstream& error_message);
 
-        std::string
-        append_all_lines_except_last_line(const std::string &chunk, TextWriter &tw);
+    void
+    append_mutation_chunk(
+        const std::string &message,
+        bool is_insertion,
+        TextWriter &tw);
 
-        std::tuple<std::string, bool>
-        append_all_lines(const std::string &message, TextWriter &tw);
+    std::string
+    append_all_lines_except_last_line(const std::string &chunk, TextWriter &tw);
 
-        std::tuple<std::string, std::vector<std::string>, std::size_t>
-        get_first_and_rest_lines(const std::string& chunk);
+    std::tuple<std::string, bool>
+    append_all_lines(const std::string &message, TextWriter &tw);
 
-        std::string
-        line_number();
+    std::tuple<std::string, std::vector<std::string>, std::size_t>
+    get_first_and_rest_lines(const std::string& chunk);
 
-        std::string
-        line_number(std::size_t line);
+    std::string
+    line_number();
 
-        void
-        call_test(path test_folder,
-                  path test_path,
-                  path aggregate_path,
-                  Glib::ustring source_code,
-                  const std::function<void(const TestCase& test_case)>& callback);
-    };
+    std::string
+    line_number(std::size_t line);
+
+    void
+    call_test(
+        path test_folder,
+        path test_path,
+        path aggregate_path,
+        Glib::ustring source_code,
+        const std::function<void(const TestCase& test_case)>& callback);
+};
+
 }
 
 #endif //FLASH_BASELINE_TEST_RUNNER_H
